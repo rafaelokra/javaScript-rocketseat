6ae91f265df944837dfbaf8b2120ad90a606f528
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-05-10 23:42:23 -0300

    estudando funcoes

---
+++ functions/functions1.js
@@ -0,0 +1,15 @@
+// criar um aplicativo de frases motivacionais
+
+// declaration - declaraçao da funçao
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciencia e persistencia')
+    console.log('Revisao e mae do aprendizado')
+}
+
+//exercutar a funcao
+// execute , run ,call . invoke
+
+createPhrases()
+
+console.log('Fim do programa')
